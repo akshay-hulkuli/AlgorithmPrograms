4fcc6c41f14c8b9643ae7f337433e2d6ca4a254e
Akshaya HP <akshayhulkuli@gmail.com>
2021-09-02 22:12:01 +0530

    ADDED: string permutations finder

--- src/com/bridgelabz/algorithmprograms/StringPermutations.java
+++ src/com/bridgelabz/algorithmprograms/StringPermutations.java
@@ -1,13 +1,14 @@
 package com.bridgelabz.algorithmprograms;
 
 import java.util.ArrayList;
+import java.util.*;
 
 public class StringPermutations {
-	private static ArrayList<String> permutations = new ArrayList<String>();
-	
+	private static ArrayList<String> permutations1 = new ArrayList<String>();
+	private static ArrayList<String> permutations2 = new ArrayList<String>();
 	private static void findPermutationsRecursively(String current , String ans) {
 		if(current.length() == 0) {
-			permutations.add(ans);
+			permutations1.add(ans);
 		}
 		for(int i=0; i<current.length();i++) {
 			char ch = current.charAt(i);
@@ -16,13 +17,43 @@ public class StringPermutations {
 		}
 	}
 	
-	private static void findPermutationsIteratively(String str) {
-		
+	private static void findPermutationIteratively(String current) 
+	{
+			int max = fact(current.length());
+			int divisor;
+			
+			for(int i=0;i<max;i++) {
+				StringBuilder str = new StringBuilder(current);
+				divisor = current.length();
+				StringBuilder ans = new StringBuilder();
+				int k = i;
+				for(int j= divisor;j>0;j--) {
+					int index = k%j;
+					k = k/j;
+					ans.append(str.charAt(index));
+					str.deleteCharAt(index);
+				}
+				permutations2.add(ans.toString());
+			}
+	
 	}
+	private static int fact(int n) {
+		if(n==0) return 1;
+		else return n*fact(n-1);
+	}
+		
 	public static void main(String[] args) {
 		String inputString = "ABC";
 		findPermutationsRecursively(inputString, "");
-		System.out.println(permutations);
+		findPermutationIteratively(inputString);
+		System.out.println(permutations1);
+		System.out.println(permutations2);
+		Collections.sort(permutations2);
+		Collections.sort(permutations1);
+		System.out.println("Are they equal : "+permutations2.equals(permutations1));
 	}
 
 }
+
+
+
